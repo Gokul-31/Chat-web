3e4903717242e5611c36292ee86a0aa5362250ec
Gokul P <gokul.pg31@gmail.com>
2021-06-23 12:30:57 +0530

    no warning

--- src/App.js
+++ src/App.js
@@ -27,7 +27,9 @@ function App() {
 
   return (
     <div className="App">
-      <header></header>
+      <header>
+        <SignOut />
+      </header>
       <section>{user ? <ChatRoom /> : <SignIn />}</section>
     </div>
   );
